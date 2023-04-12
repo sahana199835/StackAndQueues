f0a6f08244282722cbe263973421c3e95df7a075
sahana199835 <tnsahana1@gmail.com>
2023-04-12 11:57:51 +0530

    add:check dequeue from beginning

--- src/com/bridgelabz/stacksandqueues/StacksAndQueues.java
+++ src/com/bridgelabz/stacksandqueues/StacksAndQueues.java
@@ -7,54 +7,23 @@ import java.util.Stack;
 
 public class StacksAndQueues {
 	
-	static void pushData(Stack stack, int a) {
-		stack.push(new Integer(a));
-		System.out.println("Push(" + a + ")");
-		System.out.println("Stack :" + stack);
-	}
-
-	static void popData(Stack stack) {
-
-		System.out.println("pop ->");
-		Integer a = (Integer) stack.pop();
-		System.out.println(a);
-		System.out.println("Stack : " + stack);
-	}
-
-
 	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<>();
-		System.out.println("Stack :" + stack);
-
-		pushData(stack, 56);
-		pushData(stack, 30);
-		pushData(stack, 70);
-
-		System.out.println("Elements of the stack is " + stack);
-		popData(stack);
-		popData(stack);
-		popData(stack);
 		Queue<Integer> queue = new LinkedList<>();
-
-		System.out.println("Check the stack is empty or not ? " + stack.empty());
 		queue.add(56);
 		queue.add(30);
 		queue.add(70);
 
-		try {
-			stack.pop();
-		} catch (EmptyStackException e) {
-			System.out.println("Stack is empty");
-		}
-
-		try {
-			stack.peek();
-		} catch (EmptyStackException e) {
-			System.out.println("Stack is empty");
-		}
 		System.out.println("The Queue elements are : " + queue);
 		System.out.println("The Peek element is :" + queue.peek());
 
+
+		int number1 = queue.remove();
+		System.out.println("After Removing the Element : " + number1);
+
+		int number2 = queue.remove();
+		System.out.println("After Removing the Element : " + number2);
+		System.out.println("The Queue elements are : " + queue);
 	}
 
 }
+ 
