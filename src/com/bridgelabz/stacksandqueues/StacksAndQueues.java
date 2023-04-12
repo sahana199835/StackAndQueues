c0956ceef3a4300114ab1edcc0adc03d548c0f56
sahana199835 <tnsahana1@gmail.com>
2023-04-12 11:47:43 +0530

    add:check the pop and peek elements

--- src/com/bridgelabz/stacksandqueues/StacksAndQueues.java
+++ src/com/bridgelabz/stacksandqueues/StacksAndQueues.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.stacksandqueues;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class StacksAndQueues {
@@ -7,21 +8,48 @@ public class StacksAndQueues {
 	static void pushData(Stack stack, int a ) {
 		 stack.push(new Integer(a));
 		 System.out.println("Push(" + a + ")");
+		 System.out.println("Stack: " + stack);
+		
 
 	 }
 
-	 public static void main(String[] args) {
-		Stack stack = new Stack();
+	static void popData(Stack stack) {
+
+		System.out.println("pop ->");
+		Integer a = (Integer) stack.pop();
+		System.out.println(a);
+		System.out.println("Stack : " + stack);
+	}
+
+
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<>();
 		System.out.println("Stack :" + stack);
 
-		stack.push(70);
-		stack.push(30);
-		stack.push(56);
+		pushData(stack, 56);
+		pushData(stack, 30);
+		pushData(stack, 70);
 
-		System.out.println("Elements stored in stack : " + stack);
-		System.out.println("Top element of stack is : " + stack.peek());
+		System.out.println("Elements of the stack is " + stack);
+		popData(stack);
+		popData(stack);
+		popData(stack);
+
+		System.out.println("Check the stack is empty or not ? " + stack.empty());
+
+		try {
+			stack.pop();
+		} catch (EmptyStackException e) {
+			System.out.println("Stack is empty");
+		}
+
+		try {
+			stack.peek();
+		} catch (EmptyStackException e) {
+			System.out.println("Stack is empty");
+		}
+
 	}
 }
-
 
 
